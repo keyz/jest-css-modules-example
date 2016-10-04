372ed5df0f421080e94225781f23f47da0507f5a
Keyan Zhang <kz5@indiana.edu>
2016-10-04 17:31:30 -0400

    improved config

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
 import styles from './App.css';
+import logo from './logo.svg';
+import data from './sampleData.json';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div className={styles.root}>
-        <h1 className={styles.hello}>Hello, world!</h1>
+      <div className={styles.app}>
+        <div className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <h2>Welcome to React, {data.name}!</h2>
+        </div>
+        <p className={styles.intro}>
+          To get started, edit <code>src/components/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
 }
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,30 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    './src/index'
+    './src/index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'src'),
     }, {
       test: /\.css$/,
       loaders: [
         'style-loader',
         'css-loader?modules&localIdentName=[name]__[local]__[hash:base64:5]',
       ],
-    }]
-  }
+    }, {
+      test: /\.json$/,
+      loader: 'json',
+    }, {
+      test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+      loader: 'file',
+    }],
+  },
 };
